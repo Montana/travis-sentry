3fc80018246b601e288ddcf818663d94dca9c36b
Michael Mendy <montana@linux.com>
2025-05-21 08:30:27 -0700

    add breadcrumbs

--- app.js
+++ app.js
@@ -12,15 +12,33 @@ const port = process.env.PORT || 3000;
 app.use(Sentry.Handlers.requestHandler());
 
 app.get('/', (req, res) => {
+  Sentry.addBreadcrumb({
+    category: 'route',
+    message: 'User accessed home page',
+    level: 'info',
+  });
   res.send('Hello from Node + Sentry!');
 });
 
 app.get('/error', (req, res) => {
-  throw new Error('Test error for Sentry');
+  Sentry.addBreadcrumb({
+    category: 'custom',
+    message: 'About to throw a test error',
+    level: 'warning',
+  });
+
+  Sentry.addBreadcrumb({
+    category: 'user',
+    message: 'Simulated user ID: 1234',
+    data: { user_id: 1234, action: 'trigger_error' },
+    level: 'info',
+  });
+
+  throw new Error('Test error for Sentry with breadcrumbs');
 });
 
 app.use(Sentry.Handlers.errorHandler());
 
-app.listwaen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
