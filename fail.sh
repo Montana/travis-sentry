057e9eaed77dc7538024c9608def0f3cf302f5f2
Michael Mendy <montana@linux.com>
2025-05-21 08:57:21 -0700

    fail on purpose of error logging test

---
+++ fail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+START_TIME=$(date +%s)
+
+VERSION=$(sentry-cli releases propose-version)
+
+sentry-cli releases new "$VERSION"
+sentry-cli releases set-commits --auto "$VERSION"
+sentry-cli releases finalize "$VERSION"
+sentry-cli releases deploys "$VERSION" new -e production
+
+exit 1
+
+END_TIME=$(date +%s)
+DURATION=$((END_TIME - START_TIME))
+
+echo "App version $VERSION deployed in $DURATION seconds"
